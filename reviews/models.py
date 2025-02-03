5591e233126338b42127ab76656d94f171954ddc
rhtm123 <maurya.iitk@gmail.com>
2025-02-03 20:29:11 +0530

    v0.0.3

--- reviews/models.py
+++ reviews/models.py
@@ -21,4 +21,4 @@ class Review(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return  self.title if self.title else "-"
