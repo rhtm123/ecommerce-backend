0838249366201805640762ace6b79714f82e5961
rhtm123 <maurya.iitk@gmail.com>
2025-02-03 20:26:44 +0530

    v0.0.3 review title added

--- reviews/models.py
+++ reviews/models.py
@@ -19,3 +19,6 @@ class Review(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
