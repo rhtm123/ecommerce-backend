8a84903246c7db1fad7367895f704445f0935028
rhtm123 <maurya.iitk@gmail.com>
2025-07-12 19:30:59 +0530

    deleted signals

--- search/signals.py
+++ search/signals.py
@@ -3,10 +3,10 @@ from django.dispatch import receiver
 from products.models import ProductListing
 from .documents import ProductListingDocument
 
-@receiver(post_save, sender=ProductListing)
-def update_document(sender, instance, **kwargs):
-    ProductListingDocument().update(instance)
+# @receiver(post_save, sender=ProductListing)
+# def update_document(sender, instance, **kwargs):
+#     ProductListingDocument().update(instance)
 
-@receiver(post_delete, sender=ProductListing)
-def delete_document(sender, instance, **kwargs):
-    ProductListingDocument().delete(instance)
+# @receiver(post_delete, sender=ProductListing)
+# def delete_document(sender, instance, **kwargs):
+#     ProductListingDocument().delete(instance)
